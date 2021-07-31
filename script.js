3e0dc0d4bd53ed49736cee2a36947097d92142e8
Gilreon <58603241+Gilreon@users.noreply.github.com>
2021-07-31 01:31:38 -0500

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+menu
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('show');
+    menu.classList.toggle('show');
+});
